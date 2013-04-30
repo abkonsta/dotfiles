6dcf9b1ba1bda166c8988b4d59624c31efbfa1bf
Andre Konstantinov <abkonsta@gmail.com>
2013-04-29 19:33:15 -0500

    Add config file for AWS

---
+++ bash_profile_includes/aws.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#export JAVA_HOME="$(/usr/libexec/java_home)"
+#export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
+#export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
+#export AWS_RDS_HOME="/usr/local/Cellar/rds-command-line-tools/1.12.002/libexec"
